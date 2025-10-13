204b037bd15962c5104abb7efb947237f3627cda
yomnaeisa204 <yomnaheiba35@gmail.com>
2025-10-14 01:22:26 +0300

    adding action bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.ads.api)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,5 +67,6 @@ dependencies {
 
     //Google adMob
     implementation("com.google.android.gms:play-services-ads:24.7.0")
+
 
 }
